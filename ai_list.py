82f3f5a764e362c1124ae23ec04e26db53554cd0
robertoweb <86133094+web-roberto@users.noreply.github.com>
2025-12-30 14:49:26 +0100

    Adjust column width in ai_list.py

--- ai_list.py
+++ ai_list.py
@@ -10,7 +10,7 @@ st.snow()
 st.toast("Loading...")
 
 
-col1, col2, col3= st.columns([10,2,2], gap="small", vertical_alignment="top", border=True, width="stretch")
+col1, col2, col3= st.columns([9,2,2], gap="small", vertical_alignment="top", border=True, width="stretch")
 
 with col1:
     st.success("⭐⭐⭐ Top App ->  MULTICHAT MULTIUSER -> Tecnologies....")
